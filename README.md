3feae5f9904bc33bfe7761f5768b472b88dedfc0
Daniel Bloom <daniel@redteamarts.com>
2023-11-27 16:24:30 -0500

    updated readme

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
 # EmailHunter
-A tool to hunt for email addresses based on regex and google dorks. Requires SerpAPI API key.
+
+## Overview
+
+EmailHunter is a powerful and efficient tool designed to scrape email addresses from web pages. It leverages regular expressions (regex) and Google dorks (search queries) to find emails associated with a specific domain. This tool is particularly useful for gathering email addresses for business, research, or networking purposes. *It requires a SerpAPI API key to function.*
+
+## Features
+- Domain-Specific Email Hunting: Focuses on a specified domain to gather relevant email addresses.
+- Regex-Based Filtering: Uses regular expressions to match and filter email addresses and local parts.
+- Google Dorks Integration: Employs Google dorks for efficient and targeted web scraping.
+- Concurrent Processing: Utilizes multi-threading for faster processing of multiple web pages.
+- CSV Output: Outputs the gathered emails into a CSV file for easy use and analysis.
+
+## Installation
+To use EmailHunter, you need Python installed on your system. Clone the repository and install the required packages:
+
+```
+$ git clone https://github.com/bcdannyboy/EmailHunter.git
+$ cd EmailHunter
+$ pip install -r requirements.txt
+```
+
+## Basic Usage
+
+`python hunter.py -k YOUR_SERPAPI_KEY -d TARGET_DOMAIN -r regex.ini`
+
+- `-k` or `--api_key`: Your SerpAPI key.
+- `-d` or `--domain`: The target domain to search for email addresses.
+- `-r` or `--regex_config`: Path to the regex configuration file.
+
+### Example
+
+`python hunter.py -k abc123xyz -d bcdefense.com -r regex.ini`
+
+This command will search for email addresses that belong to the bcdefense.com domain, using the regex patterns defined in regex.ini.
+
+## regex.ini File
+
+The regex.ini file is a crucial component of EmailHunter. It contains two regular expressions:
+
+- `email`: Defines the pattern for matching email addresses. Example: `[^@]+@bcdefense.com` matches any string followed by @bcdefense.com.
+- `local`: Specifies the pattern for the local part (before the @ symbol) of the email address. Example: `[A-Za-z0-9]{6}` matches any alphanumeric string of exactly 6 characters.
+
+### Customizing regex.ini
+
+You can modify regex.ini to suit your specific needs. For instance, to hunt for emails from a different domain, change the email pattern accordingly. Similarly, adjust the local pattern to match different criteria for the local part of the email addresses.
+
+## Output
+
+EmailHunter outputs the results in a CSV file named emails.csv, containing two columns: 'Email' and 'Domains'. Each row represents a unique email address and the domain(s) where it was found.
--- hunter.py
+++ hunter.py
@@ -1,89 +1,91 @@
 import requests
-import re     
+import re
 import sys
 import argparse
-from serpapi import GoogleSearch   
+from serpapi import GoogleSearch
 import csv
 from concurrent.futures import ThreadPoolExecutor
 from tqdm import tqdm
 import configparser
 
-# Arguments  
-parser = argparse.ArgumentParser()
+# Set up command-line argument parsing
+parser = argparse.ArgumentParser(description="Email Hunter: A tool to scrape emails from a given domain.")
 parser.add_argument("-k", "--api_key", required=True, help="API key for SerpAPI")
-parser.add_argument("-d", "--domain", required=True, help="Domain for email addresses")  
+parser.add_argument("-d", "--domain", required=True, help="Domain for email addresses")
 parser.add_argument("-m", "--max_results", default=100, type=int, help="Max search results")
-parser.add_argument("-r", "--regex_config", required=True, help="Regex config file")  
-args = parser.parse_args()   
+parser.add_argument("-r", "--regex_config", required=True, help="Regex config file")
+args = parser.parse_args()
 
-# Settings
-DOMAIN = args.domain      
-MAX_RESULTS = args.max_results     
-API_KEY = args.api_key   
+# Initialize settings from arguments
+DOMAIN = args.domain
+MAX_RESULTS = args.max_results
+API_KEY = args.api_key
 
-# Regex  
+# Load regex patterns from config file
 config = configparser.ConfigParser()
 config.read(args.regex_config)
 EMAIL_REGEX = config['REGEX']['email']
-LOCAL_REGEX = config['REGEX']['local']  
+LOCAL_REGEX = config['REGEX']['local']
 
-# Search term   
-search_term = f"site:*.com @{DOMAIN}"       
+# Define the search term for the Google search
+search_term = f"site:*.com @{DOMAIN}"
 
-# Email storage  
-emails = {} 
+# Initialize a dictionary to store found emails
+emails = {}
 
 def main():
+    """
+    Main function to orchestrate the email hunting process.
+    """
+    print(f"Starting email search for domain {DOMAIN} with maximum results {MAX_RESULTS}...")
 
-    # Main function  
-    print("Searching...")
-    
-    search = GoogleSearch({"api_key": API_KEY, 
-                           "engine": "google",
-                           "q": search_term,
-                           "num": MAX_RESULTS
-                          })
-                           
+    # Set up and perform the Google search
+    search = GoogleSearch({"api_key": API_KEY, "engine": "google", "q": search_term, "num": MAX_RESULTS})
     results = search.get_dict()
-    links = [link['link'] for link in results['organic_results']]  
-    
+    links = [link['link'] for link in results['organic_results']]
+
+    # Use ThreadPoolExecutor to fetch emails concurrently
     with ThreadPoolExecutor() as executor:
-        futures = [executor.submit(fetch_emails, link) for link in links]
-        for future in futures:  
-            future.result()
-            
-    # Write emails dict                
-    with open('emails.csv', 'w') as f:
-        writer = csv.writer(f) 
+        futures = [executor.submit(fetch_emails, link) for link in tqdm(links, desc="Processing links")]
+        for future in tqdm(futures, desc="Fetching emails"):
+            email_results = future.result()
+            for email, domains in email_results.items():
+                if email not in emails:
+                    emails[email] = set()
+                emails[email].update(domains)
+
+    # Write the collected emails to a CSV file
+    with open('emails.csv', 'w', newline='') as f:
+        writer = csv.writer(f)
         writer.writerow(['Email', 'Domains'])
         for email, domains in emails.items():
             writer.writerow([email, ', '.join(domains)])
-            
-    print("Done!")
 
-    
+    print("Email hunting complete. Results saved to emails.csv")
+
 def fetch_emails(link):
-    
-    # Extract domain  
-    domain = re.search(r"https?://([^/]+)/", link).group(1) 
-    
-    # Fetch page  
+    """
+    Fetch and extract emails from a given link.
+    """
+    # Extract the domain from the link
+    domain = re.search(r"https?://([^/]+)/", link).group(1)
+
+    # Fetch the webpage content
     page = requests.get(link)
-    
-    # Find emails
-    found = re.findall(EMAIL_REGEX, page.text) 
-    
-    # Filter     
-    emails = {}
-    for email in found:
+
+    # Find all emails on the page using the regex pattern
+    found_emails = re.findall(EMAIL_REGEX, page.text)
+
+    # Filter and organize emails
+    local_emails = {}
+    for email in found_emails:
         local = email.split("@")[0]
         if re.match(LOCAL_REGEX, local):
-            if email not in emails:
-                emails[email] = []  
-            emails[email].append(domain)
+            if email not in local_emails:
+                local_emails[email] = set()
+            local_emails[email].add(domain)
 
-    return emails
+    return local_emails
 
-    
 if __name__ == "__main__":
     main()
