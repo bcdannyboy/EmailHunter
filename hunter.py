2afc0fc9be2e48520cc0ebd468a5450de0f39514
Daniel_Bloom <daniel@redteamarts.com>
2023-11-27 20:45:27 -0500

    added dorks

--- hunter.py
+++ hunter.py
@@ -10,59 +10,94 @@ import aiohttp
 
 # Array of google dorks
 google_dorks = [
-    '@example.com',
-    '"@example.com"',
-    'allintext: @example.com',
-    'intext:@example.com email',
-    'intitle:"example.com email"',
-    'inurl:github.com "@example.com"',
-    'site:github.com "@example.com"',
-    'site:linkedin.com "@example.com"',
+    'inurl:"@example.com"',
+    'cache:example.comsite:linkedin.com "@example.com"',
+    'intext:"@example.com" site:facebook.com',
     'site:stackoverflow.com "@example.com"',
-    'site:gitlab.com "@example.com"',
-    'site:twitter.com "@example.com"',
-    'site:facebook.com "@example.com"',
-    'site:reddit.com "@example.com"',
-    'site:medium.com "@example.com"',
-    'site:quora.com "@example.com"',
-    'filetype:pdf "@example.com"',
-    'filetype:doc "@example.com"',
-    'filetype:xls "@example.com"',
-    'filetype:txt "@example.com"',
-    'inurl:contact "@example.com"',
-    'inurl:about "@example.com"',
-    'inurl:team "@example.com"',
-    'inurl:directory "@example.com"',
-    'inurl:profiles "@example.com"',
+    'intext:@example.com email',
     'inurl:staff "@example.com"',
-    'inurl:members "@example.com"',
-    'inurl:users "@example.com"',
-    'inurl:people "@example.com"',
-    'inurl:alumni "@example.com"',
-    'inurl:faculty "@example.com"',
-    'inurl:contributors "@example.com"',
-    'inurl:emails "@example.com"',
-    'inurl:contacts "@example.com"',
-    'inurl:directory "@example.com"',
-    'site:*.example.com inurl:email',
-    'site:*.example.com inurl:contact',
-    'site:*.example.com inurl:about',
-    'site:*.example.com inurl:staff',
-    'site:*.example.com inurl:profile',
-    'site:*.example.com inurl:user',
-    'site:*.example.com inurl:team',
-    'site:*.example.com inurl:member',
-    'site:*.example.com inurl:people',
-    'site:*.example.com inurl:faculty',
-    'site:*.example.com inurl:alumni',
+    '"@example.com" intext:"contact"',
+    'intext:"@example.com" filetype:txt',
+    'intext:"@example.com" site:tumblr.com',
+    'site:example.com',
+    '@example.com',
+    'link:example.com',
+    'inurl:"contact" "@example.com"',
+    'intitle:"example.com email"',
+    'intitle:"@example.com"',
+    'filetype:txt "@example.com"',
+    'site:medium.com "@example.com"',
+    'intext:"@example.com" site:speakerdeck.com',
     'site:*.example.com inurl:contributor',
+    'inurl:team "@example.com"',
     'site:*.example.com inurl:directory',
-    'site:*.example.com inurl:emails',
-    'site:*.example.com inurl:contacts',
+    'inurl:about "@example.com"',
+    'intext:"@example.com" filetype:doc',
+    'intext:"@example.com" site:vimeo.com',
+    'inurl:people "@example.com"',
+    'inurl:profiles "@example.com"',
+    'site:*.example.com inurl:profile',
+    'related:example.com',
+    'site:twitter.com "@example.com" -"tweets"',
+    'intext:"@example.com" site:youtube.com',
     'site:*.example.com filetype:pdf',
+    'inurl:"directory" "@example.com"',
+    'inurl:users "@example.com"',
+    'filetype:doc "@example.com"',
+    'intext:"@example.com" filetype:ppt',
+    'filetype:pdf "@example.com"',
+    'site:linkedin.com "@example.com"',
+    'site:*.example.com inurl:emails',
+    'filetype:xls "@example.com"',
+    'site:*.example.com inurl:people',
+    '"@example.com" intext:"email" -site:example.com',
+    'intext:"@example.com" filetype:pdf',
+    'inurl:alumni "@example.com"',
+    'site:github.com "@example.com"',
+    'inurl:"staff" "@example.com"',
+    'intext:"@example.com" site:instagram.com',
     'site:*.example.com filetype:doc',
+    'site:*.example.com inurl:email',
+    'site:*.example.com inurl:team',
+    'inurl:"about us" "@example.com"',
+    'site:*.example.com inurl:member',
+    'site:zoominfo.com "@example.com"',
+    'site:*.example.com inurl:about',
+    'site:facebook.com "@example.com"',
+    'inurl:contacts "@example.com"',
+    'intext:"@example.com" site:pinterest.com',
+    'intext:"@example.com" site:researchgate.net',
+    'inurl:directory "@example.com"',
+    'intext:"@example.com" site:academia.edu',
+    'site:twitter.com "@example.com"',
+    'inurl:members "@example.com"',
+    'intext:"@example.com" site:slideshare.net',
+    'site:*.example.com inurl:contacts',
+    'inurl:contact "@example.com"',
+    'site:*.example.com inurl:contact',
+    'intext:"@example.com" filetype:xls',
+    'site:rocketreach.co "@example.com"',
+    '"@example.com" -site:example.com -site:mail.example.com',
+    'site:gitlab.com "@example.com"',
+    '"@example.com"',
+    'site:crunchbase.com "@example.com"',
     'site:*.example.com filetype:xls',
+    'site:*.example.com inurl:faculty',
     'site:*.example.com filetype:txt',
+    'inurl:emails "@example.com"',
+    'inurl:contributors "@example.com"',
+    'site:*.example.com inurl:alumni',
+    'inurl:"@example.com" filetype:csv',
+    'inurl:github.com "@example.com"',
+    'filetype:xlsx "@example.com"',
+    'inurl:faculty "@example.com"',
+    'site:*.example.com inurl:staff',
+    'site:*.example.com inurl:user',
+    '"@example.com" -site:example.com',
+    'intext:"@example.com" site:reddit.com',
+    'site:reddit.com "@example.com"',
+    'site:quora.com "@example.com"',
+    'allintext: @example.com'
 ]
 
 # Set up command-line argument parsing
@@ -118,10 +153,10 @@ def organize_emails(emails, domain):
 
 def write_emails_to_csv(filename, email_dict):
     """ Write emails to a CSV file. """
-    with open(filename, 'w', newline='') as f: 
+    with open(filename, 'w', newline='') as f:
         writer = csv.writer(f)
         writer.writerow(['Email', 'Domains'])
-        
+
         for email, domains in email_dict.items():
             writer.writerow([email, ', '.join(domains)])
 
@@ -133,45 +168,38 @@ async def main_async():
     print("Beginning search queries...")
 
     async with aiohttp.ClientSession() as session:
-        with ThreadPoolExecutor() as executor:
-            loop = asyncio.get_event_loop()
-            tasks = []
+        tasks = []
 
-            # Function to run serpapi searches in the executor
-            def run_search(term):
-                print(f"Searching for {term}...")
-                search = GoogleSearch({"api_key": API_KEY, "engine": "google", "q": term, "num": MAX_RESULTS})
-                results = search.get_dict()
-                links = [link['link'] for link in results.get('organic_results', [])]
-                return links
+        # Function to run serpapi searches in a separate thread
+        def run_search(term):
+            search = GoogleSearch({"api_key": API_KEY, "engine": "google", "q": term, "num": MAX_RESULTS})
+            results = search.get_dict()
+            links = [link['link'] for link in results.get('organic_results', [])]
+            return links
 
-            # Asynchronously schedule all serpapi searches and fetches
-            for term in search_terms:
-                # Schedule serpapi search
-                search_task = loop.run_in_executor(executor, run_search, term)
-                links = await search_task  # Wait for the search to complete
+        # Running the serpapi search synchronously for each term
+        for term in search_terms:
+            links = run_search(term)
 
-                # Schedule email fetches for each link
-                for link in links:
-                    fetch_task = loop.run_in_executor(executor, fetch_emails, session, link)
-                    print(f"Scheduled fetch for {link}")
-                    tasks.append(fetch_task)
+            # Schedule email fetches for each link asynchronously
+            for link in links:
+                task = fetch_emails(session, link)
+                tasks.append(task)
 
-            # Wait for all tasks to complete
-            print(f"Waiting for {len(tasks)} tasks to complete...")
-            results = await asyncio.gather(*tasks)
+        # Wait for all fetch tasks to complete
+        results = await asyncio.gather(*tasks)
 
-            # Process results
-            for exact, found in results:
-                for email, domains in exact.items():
-                    if email not in exact_emails:
-                        exact_emails[email] = set()
-                    exact_emails[email].update(domains)
+        # Process results
+        for exact, found in results:
+            for email, domains in exact.items():
+                if email not in exact_emails:
+                    exact_emails[email] = set()
+                exact_emails[email].update(domains)
 
-                for email, domains in found.items():
-                    if email not in found_emails:
-                        found_emails[email] = set()
-                    found_emails[email].update(domains)
+            for email, domains in found.items():
+                if email not in found_emails:
+                    found_emails[email] = set()
+                found_emails[email].update(domains)
 
     # Write results to CSV files
     write_emails_to_csv('exact_matches.csv', exact_emails)
